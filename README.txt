57916231aa3ce59644131b10555cb648c756dd36
NateWilliams2 <Williams2@grinnell.edu>
2019-11-04 00:05:03 -0600

    Assignment complete

--- README.txt
+++ README.txt
@@ -10,32 +10,147 @@ and deletion functions and should maintain the red-black tree properties.
 
 2. Analysis
 The classes are
-    -node:
-    This class implements a single node of the rb-tree and stores a parent, left and right child, a key, and a color,
+    -rb_node:
+    This class implements a single rb_node of the rb-tree and stores a parent, left and right child, a key, and a color,
     represented by a boolean value.
-    Node also has the methods
-        -print_level, which prints all sub-nodes of a given height.
+    rb_node also has the methods
+        -print_level, which prints all sub-rb_nodes of a given height.
         -get_color, which translates the boolean color value into a string for printing.
-        -get-minimum, which returns the smallest-key node in node's sub-tree.
+        -get-minimum, which returns the smallest-key rb_node in rb_node's sub-tree.
     -nil:
     This class is my way of implementing the nil value for rb-trees. It contains a parent and left and right children
     and a key which should always be set to None, as well as a color which should always be black. It serves as an empty
-    node class which I found easier to separate into its own class so that node children can be set to nil by default.
+    rb_node class which I found easier to separate into its own class so that rb_node children can be set to nil by default.
     -tree:
-    This class implements the actual rb-tree data structure. It stores only a root node.
+    This class implements the actual rb-tree data structure. It stores only a root rb_node.
     Tree has the methods:
-        -rb_insert
-        -rb_insert_fixup
-        -rb_transplant
-        -rb_delete
-        -rb_delete_fixup
-        -left_rotate
-        -right_rotate
-        -find_node
-        -print_tree
+        -rb_insert: inserts a given rb_node into the tree.
+            This function contains a loop which iterates on at most every level of the tree, taking O(height) = O(logn) time.
+            It also calls rb_insert_fixup, which similarly has a loop whose number of iterations is based on the height.
+        -rb_insert_fixup: returns tree to red-black properties after an insert
+            As mentioned above, this function contains a loop which looks two rb_nodes above the current rb_node and iterates again.
+            It is therefore O(height), or O(logn)
+        -rb_transplant: transplants one rb_node for another
+            This function is constant-time, because it has no loops and every function it calls is constant
+        -rb_delete: deletes a given rb_node from the tree.
+            This function has no loops but calls the rb_delete_fixup function, so its time is O(logn)
+        -rb_delete_fixup: returns tree to red-black properties after an insert
+            The only non-constant part of this function is a loop which repeats moving a pointer up the tree. It therefore
+            loops based on the height of the tree in the worst case, so it is O(height) or O(logn).
+        -left_rotate: rotates given rb_node and children to the left
+            This function is constant-time, because it has no loops and every function it calls is constant
+        -right_rotate: rotates given rb_node and children to the right
+            This function is constant-time, because it has no loops and every function it calls is constant
+        -find_rb_node: finds a rb_node given a value
+            Just like in a binary tree, this function is O(logn) because it stops once at each height of the tree.
+        -print_tree: prints the entire tree in human-readable format
+            This function is O(n) because it visits and prints every rb_node once.
 
 The following methods are also defined:
--get_height
--get_black_height
--initialize
--
+-get_height: gets the height of a given rb_node
+    This function is O(n) because it visits every rb_node once.
+-get_black_height: gets the black-height of a given rb_node
+    This function is O(n) because it visits every rb_node once.
+-initialize: initializes a tree with 10 pseudo-random values based on lfsr initialized with a given seed.
+
+3. Instructions:
+To run this code, make sure you have version 3 of python installed and simply run python red_black_tree.py .
+You will then be prompted to enter a seed or simply use the default. Follow the command line instructions to execute
+tree commands.
+
+4. Sample I.O:
+Enter an integer below 256 to seed rb_nodes. Enter -1 to use default seed value.
+-1
+Initializing tree...
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (161, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+ADD
+What value would you like to add?
+122
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (122, red) (161, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+ADD
+What value would you like to add?
+30
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (30, red) (122, red) (161, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+ADD
+What value would you like to add?
+344
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (30, red) (122, red) (161, red) (344, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+DEL
+What value would you like to delete?
+30
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (122, red) (161, red) (344, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+DEL
+What value would you like to delete?
+122
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (161, red) (344, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+DEL
+What value would you like to delete?
+344
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (161, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+DEL
+What value would you like to delete?
+22
+a node with that value does not exist
+(122, black)
+(61, red) (233, red)
+(30, black) (67, black) (135, black) (244, black)
+(15, red) (161, red)
+Enter ADD, DEL, BLKH, or anything else to quit
+BLKH
+What value's black height would you like to find?
+122
+rb_node height is: 2
+Enter ADD, DEL, BLKH, or anything else to quit
+BLKH
+What value's black height would you like to find?
+67
+rb_node height is: 1
+Enter ADD, DEL, BLKH, or anything else to quit
+BLKH
+What value's black height would you like to find?
+15
+rb_node height is: 0
+Enter ADD, DEL, BLKH, or anything else to quit
+BLKH
+What value's black height would you like to find?
+1
+a node with that value does not exist
+Enter ADD, DEL, BLKH, or anything else to quit
+QUIT
+
+Process finished with exit code 0
+
+5. My Experience
+This assignment reminded my of many CS-207 assignments, because it was an implementation rather than a problem solving
+assignment. I found myself spending a lot of my time reviewing pseudocode from the textbook and implementing it in python.
+I found the LFSR program to be an interesting aside from that: rather than a lot of implementation code, it was fairly
+simple but required more intentional thought and problem solving before-hand. Overall the most frustrating part of this
+assignment was tracking down bugs that were there because of mistakes I made when interpreting pseudocode.
--- red_black_tree.py
+++ red_black_tree.py
@@ -16,62 +16,62 @@ class tree:
     def __init__(self):
         self.root = NIL
     
-    #inserts a node into rb-tree
-    def rb_insert(self, node):
+    #inserts a rb_node into rb-tree
+    def rb_insert(self, rb_node):
         y = NIL
         x = self.root
         while x != NIL:
             y = x
-            if node.key < x.key:
+            if rb_node.key < x.key:
                 x = x.left 
             else: 
                 x = x.right 
-        node.parent = y
+        rb_node.parent = y
         if y == NIL:
-            self.root = node
-        elif node.key < y.key:
-            y.left = node
+            self.root = rb_node
+        elif rb_node.key < y.key:
+            y.left = rb_node
         else:
-            y.right = node
-        node.left = NIL
-        node.right = NIL
-        node.color = RED
-        self.rb_insert_fixup(node)
+            y.right = rb_node
+        rb_node.left = NIL
+        rb_node.right = NIL
+        rb_node.color = RED
+        self.rb_insert_fixup(rb_node)
 
     #fixes rb tree post-insert to ensure it still follows rb-tree properties
-    def rb_insert_fixup(self, node):
-        while node.parent.color == RED:
-            if node.parent == node.parent.parent.left:
-                y = node.parent.parent.right
+    def rb_insert_fixup(self, rb_node):
+        while rb_node.parent.color == RED:
+            if rb_node.parent == rb_node.parent.parent.left:
+                y = rb_node.parent.parent.right
                 if y.color:
-                    node.parent.color = BLACK
+                    rb_node.parent.color = BLACK
                     y.color = BLACK
-                    node.parent.parent.color = RED
-                    node = node.parent.parent
+                    rb_node.parent.parent.color = RED
+                    rb_node = rb_node.parent.parent
                 else:
-                    if node == node.parent.right:
-                        node = node.parent
-                        self.left_rotate(node)
-                    node.parent.color = BLACK
-                    node.parent.parent.color = RED
-                    self.right_rotate(node.parent.parent)
-            elif node.parent == node.parent.parent.right:
-                y = node.parent.parent.left
+                    if rb_node == rb_node.parent.right:
+                        rb_node = rb_node.parent
+                        self.left_rotate(rb_node)
+                    rb_node.parent.color = BLACK
+                    rb_node.parent.parent.color = RED
+                    self.right_rotate(rb_node.parent.parent)
+            elif rb_node.parent == rb_node.parent.parent.right:
+                y = rb_node.parent.parent.left
                 if y.color:
-                    node.parent.color = BLACK
+                    rb_node.parent.color = BLACK
                     y.color = BLACK
-                    node.parent.parent.color = RED
-                    node = node.parent.parent
+                    rb_node.parent.parent.color = RED
+                    rb_node = rb_node.parent.parent
                 else:
-                    if node is node.parent.left:
-                        node = node.parent
-                        self.right_rotate(node)
-                    node.parent.color = BLACK
-                    node.parent.parent.color = RED
-                    self.left_rotate(node.parent.parent)
+                    if rb_node is rb_node.parent.left:
+                        rb_node = rb_node.parent
+                        self.right_rotate(rb_node)
+                    rb_node.parent.color = BLACK
+                    rb_node.parent.parent.color = RED
+                    self.left_rotate(rb_node.parent.parent)
         self.root.color = BLACK
 
-    #transplants node u with node v
+    #transplants rb_node u with rb_node v
     def rb_transplant(self, u, v):
         if u.parent == NIL:
             self.root = v
@@ -81,7 +81,7 @@ class tree:
             u.parent.right = v
         v.parent = u.parent
     
-    #deletes given node from tree
+    #deletes given rb_node from tree
     def rb_delete(self, z):
         y = z
         y_original_color = z.color
@@ -93,7 +93,6 @@ class tree:
             self.rb_transplant(z, z.left)
         else:
             y = z.right.get_minimum()
-            print(y.key, y.color)
             y_original_color = y.color
             x = y.right 
             if y.parent == z:
@@ -157,7 +156,7 @@ class tree:
         x.color = BLACK
 
                 
-    #rotates tree left around node x
+    #rotates tree left around rb_node x
     def left_rotate(self, x):
         y = x.right
         x.right = y.left
@@ -173,7 +172,7 @@ class tree:
         y.left = x
         x.parent = y
 
-    #rotates tree right around node x
+    #rotates tree right around rb_node x
     def right_rotate(self, x):
         y = x.left
         x.left = y.right
@@ -189,27 +188,27 @@ class tree:
         y.right = x
         x.parent = y
 
-    #finds a node in tree based on a given value
-    def find_node(self, val):
-        node = self.root
-        while node != NIL:
-            if val == node.key:
-                return node
-            elif val < node.key:
-                node = node.left
-            elif val > node.key: 
-                node = node.right
+    #finds a rb_node in tree based on a given value
+    def find_rb_node(self, val):
+        rb_node = self.root
+        while rb_node != NIL:
+            if val == rb_node.key:
+                return rb_node
+            elif val < rb_node.key:
+                rb_node = rb_node.left
+            elif val > rb_node.key:
+                rb_node = rb_node.right
         return NIL
 
-    #level-order print of tree's nodes
+    #level-order print of tree's rb_nodes
     def print_tree(self):
         for level in range (1, get_height(self.root) + 1):
             self.root.print_level(level)
             print("")
     
-#node of a rb-tree
-class node:
-    #initializes node with defaults: no relatives, key=0, color=red
+#rb_node of a rb-tree
+class rb_node:
+    #initializes rb_node with defaults: no relatives, key=0, color=red
     def __init__(self, key = 0):
         self.parent = NIL
         self.key = key
@@ -218,7 +217,7 @@ class node:
         self.right = NIL 
         self.left = NIL
 
-    #recursively prints a givel level of child nodes of node
+    #recursively prints a givel level of child rb_nodes of rb_node
     def print_level(self, level):
         if level == 1: 
             print("(" + str(self.key) + ", " + self.get_color(), end = ') ') 
@@ -228,55 +227,55 @@ class node:
             if self.right != NIL: 
                 self.right.print_level(level - 1)
 
-    #gets the color of a node in a human-readable string
+    #gets the color of a rb_node in a human-readable string
     def get_color(self):
         if self.color:
             return "red"
         else:
             return "black"
 
-    #gets the minimum node from a subtree
+    #gets the minimum rb_node from a subtree
     def get_minimum(self):
-        node = self
-        while node.left != NIL:
-            node = node.left
-        return node
+        rb_node = self
+        while rb_node.left != NIL:
+            rb_node = rb_node.left
+        return rb_node
 
-#gets the un-colored height of a node
-def get_height(node):
-    if node == NIL:
+#gets the un-colored height of a rb_node
+def get_height(rb_node):
+    if rb_node == NIL:
         return 0
     else:
-        left_height = get_height(node.left)
-        right_height = get_height(node.right)
+        left_height = get_height(rb_node.left)
+        right_height = get_height(rb_node.right)
         return max(left_height, right_height) + 1
 
-#gets the black height of a node
-def get_black_height(node):
-    if node == NIL:
+#gets the black height of a rb_node
+def get_black_height(rb_node):
+    if rb_node == NIL:
         return 0
     else:
-        left_height = get_black_height(node.left)
-        right_height = get_black_height(node.right)
-        if node.color:
+        left_height = get_black_height(rb_node.left)
+        right_height = get_black_height(rb_node.right)
+        if rb_node.color:
             return max(left_height, right_height)
         else:
             return max(left_height, right_height) + 1
 
 #initializes tree with random values
 def initialize(seed = None):
-    new_node = node()
+    new_rb_node = rb_node()
     if seed == None:
         gen = lfsr()
     else:
         gen = lfsr(seed)
     t = tree()
     for i in range (0, 9):
-        t.rb_insert(node(gen.nextValue()))
+        t.rb_insert(rb_node(gen.nextValue()))
     return t
 
 #runs program in indefinite loop with user input
-user_input = int(input("Enter an integer below 256 to seed nodes or anything else for the default seed\n"))
+user_input = int(input("Enter an integer below 256 to seed rb_nodes. Enter -1 to use default seed value.\n"))
 if user_input >= 0 and user_input < 256:
     print("Initializing tree...")
     t = initialize(user_input)
@@ -289,24 +288,24 @@ while 1:
         "Enter ADD, DEL, BLKH, or anything else to quit\n")
     if user_input == "ADD":
         user_input = int(input("What value would you like to add?\n"))
-        new_node = node(user_input)
-        t.rb_insert(new_node)
+        new_rb_node = rb_node(user_input)
+        t.rb_insert(new_rb_node)
         t.print_tree()
     elif user_input == "DEL":
         user_input = int(input("What value would you like to delete?\n"))
-        node = t.find_node(user_input)
-        if node != NIL and node != None:
-            t.rb_delete(node)
+        rb_node = t.find_rb_node(user_input)
+        if rb_node != NIL and rb_node != None:
+            t.rb_delete(rb_node)
         else:
-            print("Node does not exist")
+            print("a node with that value does not exist")
         t.print_tree()
     elif user_input == "BLKH":
         user_input = int(input("What value's black height would you like to find?\n"))
-        node = t.find_node(user_input)
-        if node != NIL and node != None:
-            print("Node height is: " + str(get_black_height(node)))
+        rb_node = t.find_rb_node(user_input)
+        if rb_node != NIL and rb_node != None:
+            print("rb_node height is: " + str(get_black_height(rb_node)))
         else:
-            print("Node does not exist")
+            print("a node with that value does not exist")
     else:
         break
 
